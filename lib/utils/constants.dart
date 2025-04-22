eff7fc0b59a811e79632a8b962881c421f14b50a
AliHere76 <mu.ali.7645@gmail.com>
2025-04-23 03:02:14 +0500

    Cloudinary Setup

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String cloudinaryCloudName =
+      'djnqejhoj'; // Replace with your Cloudinary Cloud Name
+  static const String cloudinaryUploadPreset =
+      'COMSICON'; // Replace with your Upload Preset Name
+  static const String cloudinaryFolder =
+      'COMSICON/'; // Replace with your folder structure
+}
