e20b011c69f0dda3bf134cf9891e3d4c6b467792
AliHere76 <mu.ali.7645@gmail.com>
2025-04-23 02:26:17 +0500

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:comsicon/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -11,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
